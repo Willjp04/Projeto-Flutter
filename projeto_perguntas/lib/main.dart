61dece3cddff8835cde3ea9d40f18564d0d93ce3
Willian Sostenes <sostenes655@gmail.com>
2020-12-03 21:48:43 -0300

    Inserindo botões na interface visual do app

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -5,13 +5,34 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Perguntas'),
-      ),
-      body: Text('Olá Flutter'),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas[0]),
+              RaisedButton(
+                child: Text('Resposta 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Resposta 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Resposta 3'),
+                onPressed: null,
+              )
+            ],
+          )),
+    );
   }
 }
